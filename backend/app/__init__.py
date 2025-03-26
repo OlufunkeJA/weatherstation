cf4c82531d4e5438ed8f2dde2f759972f846612b
Olufunke Ogunde <olufunkeogunde@outlook.com>
2025-03-26 16:38:17 -0500

    Backend Complete

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,11 +5,14 @@ from os import name
 from .functions import DB 
 from .mqtt import MQTT  
 
+from flask_cors import CORS  # Import CORS
+
 # Create MongoDB instance to get access to all the functions defined in functions.py
 mongo = DB(Config)
 Mqtt  = MQTT(mongo)
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config) 
 
 from app import views
